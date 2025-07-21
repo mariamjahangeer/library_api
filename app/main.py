1097f6fc93deea594e8b529bdc39ec18777998fe
mariamjahangeer <mariamjahangeer109@gmail.com>
2025-07-21 08:47:42 +0500

    chore: include book router in main app
    Main file updated to register book routes for the API.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from app.routers import book
 
 app = FastAPI()
+
+app.include_router(book.router)
 
 @app.get("/")
 def root():
     print(" API root hit!")  # Log
     return {"message": "Welcome to the Library System API"}
+
